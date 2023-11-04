999ec46522f253866da3c0934dbf528b0beeccd7
devwinn <80548344+devwinn@users.noreply.github.com>
2023-11-04 03:08:53 -0700

    functional w/ reset button. resizing still needed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,11 @@
 const gridContainer = document.getElementById('grid-container');
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', resetGrid);
+makeGrid(16);
 
 
-makeGrids(16);
-
-function makeGrids(size) {
+function makeGrid(size) {
     let squareSize = (500 / size) - 2;
     let x = 0;
     while (x < size) {
@@ -15,12 +17,30 @@ function makeGrids(size) {
         let gridRow = document.getElementsByClassName(`grid-row-${x}`);
         while (y < size) {
             let gridSquare = document.createElement('div');
-            gridSquare.id = 'grid-square';
+            gridSquare.className = 'grid-square';
             gridSquare.style.height = `${squareSize}px`;
             gridSquare.style.width = `${squareSize}px`;
             gridRow[0].appendChild(gridSquare);
             y++;
         }
         x++;
+
     }
+
+    let squares = document.getElementsByClassName('grid-square')
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('mouseover', etch);
+    }
+}
+
+function etch(e) {
+    e.target.style.background = 'black';
+}
+
+function resetGrid() {
+    let squares = document.getElementsByClassName('grid-square')
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = 'white';
+    }
+    console.log("grid resetting")
 }
